14facfb8a9ae6bae00594a4d15f720232e68caaa
Sofia15M <sofiaortizacevedo2007@gmail.com>
2024-04-21 11:39:50 -0500

    Añadiendo archivos y diretorios iniciales

---
+++ src/main/java/com/DigitalFace/DigitalFace/DigitalFaceApplication.java
@@ -0,0 +1,13 @@
+package com.DigitalFace.DigitalFace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalFaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalFaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DigitalFace/DigitalFace/DigitalFaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DigitalFace.DigitalFace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalFaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
