f7f1e85b7ccdb294939c0508a2cfd31796931bd8
ximenaortiz01 <114520559+ximenaortiz01@users.noreply.github.com>
2024-04-26 12:40:47 -0500

    Update Administrador.java

--- src/main/java/com/DigitalFace/DigitalFace/Administrador.java
+++ src/main/java/com/DigitalFace/DigitalFace/Administrador.java
@@ -1,4 +1,20 @@
 package com.DigitalFace.DigitalFace;
 
+@Entity
+@Table(name = "Administrador")
 public class Administrador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ID_Administrador;
+
+    private String Foto_Administrador;
+    private String Nombre_Administrador;
+    private int Edad_Administrador;
+    private String Cargo_Administrador;
+    private String Direccion_Administrador;
+    private String Tel_Cel_Administrador;
+    private String Tiempo_Trabajo_Administrador;
+    private String Fecha_Registro_Administrador;
+    private int ID_UNIDAD;
+    private String status;
 }
