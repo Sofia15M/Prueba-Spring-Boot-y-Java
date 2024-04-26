61d610a6f17881cb023321a42ad7e9487338e006
ximenaortiz01 <114520559+ximenaortiz01@users.noreply.github.com>
2024-04-26 12:40:29 -0500

    Update PersonalLimpieza.java

--- src/main/java/com/DigitalFace/DigitalFace/PersonalLimpieza.java
+++ src/main/java/com/DigitalFace/DigitalFace/PersonalLimpieza.java
@@ -1,4 +1,20 @@
 package com.DigitalFace.DigitalFace;
 
+@Entity
+@Table(name = "PersonalLimpieza")
 public class PersonalLimpieza {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ID_PersonalL;
+
+    private String Foto_PersonalL;
+    private String Nombre_PersonalL;
+    private int Edad_PersonalL;
+    private String Cargo_PersonalL;
+    private String Direccion_PersonalL;
+    private String Tel_Cel_PersonalL;
+    private String Tiempo_Trabajo_PersonalL;
+    private String Fecha_Registro_PersonalL;
+    private int ID_UNIDAD;
+    private String status;
 }
