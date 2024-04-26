7115024d6a3eddf8eb37bbe9f3175bfabf471028
ximenaortiz01 <114520559+ximenaortiz01@users.noreply.github.com>
2024-04-26 12:40:01 -0500

    Update Apartamentos.java

--- src/main/java/com/DigitalFace/DigitalFace/Apartamentos.java
+++ src/main/java/com/DigitalFace/DigitalFace/Apartamentos.java
@@ -1,4 +1,14 @@
 package com.DigitalFace.DigitalFace;
 
+@Entity
+@Table(name = "Apartamentos")
 public class Apartamentos {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ID_Apartamento;
+
+    private String Descripcion_Apartamento;
+    private int ID_UNIDAD;
+    private String status;
+    private String ID_Propietario;
 }
