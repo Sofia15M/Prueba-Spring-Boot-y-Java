13d52823db716ffc3e24e17db421eb568e144957
ximenaortiz01 <114520559+ximenaortiz01@users.noreply.github.com>
2024-04-26 12:39:34 -0500

    Update Roles.java

--- src/main/java/com/DigitalFace/DigitalFace/Roles.java
+++ src/main/java/com/DigitalFace/DigitalFace/Roles.java
@@ -1,4 +1,11 @@
 package com.DigitalFace.DigitalFace;
 
+@Entity
+@Table(name = "Roles")
 public class Roles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int ID_ROL;
+
+    private String  Cargo;
 }
